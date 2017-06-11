e47b9bfb0de4280c9cc9d07e6c486a7db6b02f20
EricRiney <fails.to.install@gmail.com>
2017-06-10 23:16:23 -0700

    second commit

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ericriney/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, wo   rld\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
